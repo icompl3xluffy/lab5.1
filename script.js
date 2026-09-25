c1c5d13b79892f1da9cab2d794e54c913c099ece
icompl3xluffy <bertyneganthier@yahoo.com>
2026-09-25 19:52:17 -0400

    add button working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const productNameInput = document.getElementById('product-name');
 const productPriceInput = document.getElementById('product-price');
-const addProductButton = document.getElementById('add-product');
+const addButton = document.getElementById('add-product');
 const cart = document.getElementById('cart');
 const totalPriceSpan = document.getElementById('total-price');
  
@@ -11,40 +11,40 @@ let productInfo=[]
 function updateTotalPrice(amount) {
   totalPrice += amount;
   totalPriceSpan.textContent = totalPrice.toFixed(2);
+
 }
 
- addProductButton.addEventListener('click', ()=>  {
+ addButton.addEventListener('click', ()=>  {
     if( !productNameInput.value || !productPriceInput.value){
-        alert("please enter product")
+        // alert("please enter product")
     }
     let productName = productNameInput.value
     let priceInput= productPriceInput.value
+    
     let newProduct= {
         name: productName,
         price:priceInput,
     }
+    
         productInfo.push(newProduct)
-        console.log(productInfo);
-
-        for (let i=0; i<productInfo.length; i++){
-            let item=document.createElement("li")
-                item.innerText= productInfo[i].name+ productInfo[i].price
-            console.log(productInfo[i])
-            console.log(`${productInfo[i].name}  ${productInfo[i].price}`)
-        }
-
-        
-
-
+    
+        let item = document.createElement("li");
+        item.textContent= `${newProduct.name} $${newProduct.price} `
+       
+        cart.appendChild(item)
+    
  })
 
-
-
-
-// Function to remove an item
-function removeItem(event) {
-  const item = event.target.closest('li');
-  const price = parseFloat(item.dataset.price);
-  updateTotalPrice(-price);
-  item.remove();
-}
+//  for(ul.innerhtml){
+//         let deletebtn = document.createElement('button');
+//     deletebtn.textContent="Delete";
+//     .append(productInfo)
+// //  }
+//         if()
+// // Function to remove an item
+// function removeItem(event) {
+//   const item = event.target.closest('li');
+//   const price = parseFloat(item.dataset.price);
+//   updateTotalPrice(-price);
+//   item.remove();
+// }
